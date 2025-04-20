08ac7f18b671cf2ac99670584b10fcf4728b703b
vmayres <victoroliveiraayres@gmail.com>
2025-04-19 23:24:31 -0300

    new robot description

--- build/caramelo_localization/build/lib/caramelo_localization/odometry_motion_model.py
+++
@@ -1,8 +0,0 @@
-import rclpy
-from rclpy.node import Node
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu
-
-class OdometryMotionModel(Node):
-    def __init__(self):
-        pass
